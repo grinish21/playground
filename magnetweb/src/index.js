e35462ecc07bdd45cf8e3d4a9d7732d7d5e7d1ed
Grinish <Grinish@gmail.com>
2019-02-09 15:12:03 -0700

    (chore) add bootstrap table.

--- magnetweb/src/index.js
+++ magnetweb/src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
-
+import './index.css';
 
 const store = createStore(
     rootReducer,
--- magnetweb/src/studentList.js
+++ magnetweb/src/studentList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStudents } from "./studentActions";
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 class StudentList extends React.Component {
     componentDidMount() {
@@ -19,11 +20,11 @@ class StudentList extends React.Component {
         }
 
         return (
-            <ul>
-                {students.map(student => (
-                    <li key={student.id}>{student.firstName}</li>
-                ))}
-            </ul>
+            <BootstrapTable data={students} striped={true} hover={true}>
+                <TableHeaderColumn dataField="firstName" dataAlign="center" dataSort={true}>FirstName</TableHeaderColumn>
+                <TableHeaderColumn dataField="lastName" isKey={true} dataSort={true}>LastName</TableHeaderColumn>
+                <TableHeaderColumn dataField="department" dataSort={true}>Department</TableHeaderColumn>
+            </BootstrapTable>
         );
     }
 }
